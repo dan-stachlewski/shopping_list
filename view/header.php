b7b3824e084799be5f9ffe16b522c27c4339b18b
dan-stachlewski <dan.stachlewski@gmail.com>
2015-09-26 17:08:19 +1000

    Added the structural code to the Header & Footer. Very basic HTML Framework to be updated at a l8r d8.

--- view/header.php
+++ view/header.php
@@ -1,8 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Shopping List Application</title>
+    </head>
+    <body>
+        <div id="page">
+            <div id="header">
+                <h1>Shopping List</h1>
+            </div> <!-- END header -->
+            <?php if (isset($current_user) && $current_user !== FALSE) : ?>
+                <a href="./login/index.php?action=logout">Logout</a> |
+                <a href="./login/index.php?action=show_update_form">Update Details</a>
+            <?php endif; ?>
+            <div id="errors">
+                <!-- PART 1: The Errors -->
+                <?php if (count($errors) > 0) : ?>
+                <h2>Errors</h2>
+                    <ul>
+                        <?php foreach($errors as $error) : ?>
+                            <li><?= $error; ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+            </div> <!-- END errors -->
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
